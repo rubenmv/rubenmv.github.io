28be169349c085367ceee9c98e8c8a9808a8c161
Rubén Martínez <rub3nmv@gmail.com>
2014-10-12 03:52:08 +0200

    Update TODO

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,49 @@
-
 /*global require*/
 /**
  * Minified library for DOM ready and listeners. IE legacy support
  */
 // minified.js 'require' implementation only works with minified.js and related
+(function() {
+	'use strict';
 
-var MINI = require('minified');
-var $ = MINI.$;
+	var MINI = require('minified');
+	var $ = MINI.$;
 
+	function showTop() {
+		$('#works').animate({
+				$$show: 1,
+				$$fade: 1
+			}, 300)
+			.then(function() {
+				window.location = '#top';
+			});
+	}
 
-function showTop() {
-	$('#works').animate({$$show: 1, $$fade: 1}, 300)
-	.then(function(){
-		window.location = '#top';
+	function showWorks() {
+		$('#works').animate({
+				$$show: 1,
+				$$fade: 1
+			}, 300)
+			.then(function() {
+				window.location = '#works';
+			});
+	}
+
+	function showContact() {
+		$('#works').animate({
+				$$show: 0,
+				$$fade: 0
+			}, 300)
+			.then(function() {
+				window.location = '#contact';
+			});
+	}
+
+	// Listeners
+	$.ready(function domReady() {
+		// Menu
+		$('#menu-top').onClick(showTop);
+		$('.link-works').onClick(showWorks);
+		$('#menu-contact').onClick(showContact);
 	});
-}
-function showWorks() {
-	$('#works').animate({$$show: 1, $$fade: 1}, 300)
-	.then(function(){
-		window.location = '#works';
-	});
-}
-function showContact() {
-	$('#works').animate({$$show: 0, $$fade: 0}, 300)
-	.then(function(){
-		window.location = '#contact';
-	});
-}
-
-// Listeners
-$.ready(function domReady() {
-	// Menu
-	$('#menu-top').onClick(showTop);
-	$('.link-works').onClick(showWorks);
-	$('#menu-contact').onClick(showContact);
-});
+})();
