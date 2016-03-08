3c954b79807801159d39d92dd4c91f4da9f97314
Ruben Martinez <rub3nmv@gmail.com>
2016-03-08 18:15:44 +0100

    Fixes problems with padding and css cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -14,26 +14,16 @@ gulp.task('js', function(){
   .pipe(gulp.dest('./res'));
 });
 
-// Minify JS
-/*gulp.task('uglify', function () {
-    return gulp.src('js/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('res'));
-});
-*/
 // Minify CSS
 gulp.task('css', function () {
   return gulp.src(['css/font-awesome.min.css', 'css/reset.css', 'css/grid.css', 'css/style.css'])
   .pipe(concat('style.css'))
+  .pipe(unCSS({
+            html: ['index.html']
+        }))
   .pipe(minifyCSS())
-  .pipe(gulp.dest('res'))
+  .pipe(gulp.dest('./res'))
 });
 
-/*gulp.task('minify-css', function () {
-  return gulp.src('css/*.css')
-  .pipe(minifyCSS())
-  .pipe(gulp.dest('res'))
-});*/
-
 // Default Task
-gulp.task('default', ['js', 'css']);
+gulp.task('build', ['js', 'css']);
